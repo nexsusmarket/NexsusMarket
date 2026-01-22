79832bc1599b4647cbf59286950e09200a1eb31f
Pavan Kumar <nexsusmarketshop@gmail.com>
2026-01-22 22:03:37 +0530

    Change false to True

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
     port: 465,
-    secure: false, // Must be false for 587
+    secure: true, // Must be false for 587
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
