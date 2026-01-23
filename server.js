867c58e281d146544eeef81faa167eb02a1bfb8d
Pavan Kumar <nexsusmarketshop@gmail.com>
2026-01-23 19:04:59 +0530

    Giving the brevo

--- server.js
+++ server.js
@@ -40,16 +40,15 @@ const client = new MongoClient(uri);
 
 // --- NODEMAILER TRANSPORTER ---
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // Must be false for 587
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-    family: 4,
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER, // MUST be "apikey"
+    pass: process.env.EMAIL_PASS
+  }
 });
+
 
 // ============================================================
 //  👇 JAVASCRIPT RECOMMENDATION ENGINE 👇
